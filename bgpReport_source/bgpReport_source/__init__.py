2f77f643555a1ad349aac95c2d3dedf65a2752fc
root <root@bgp-report-dev.grnoc.iu.edu>
2017-11-16 19:36:10 +0000

    Code refactor to comply with PEP8 python styling guide

--- bgpReport_source/bgpReport_source/__init__.py
+++ bgpReport_source/bgpReport_source/__init__.py
@@ -1,87 +1,97 @@
-import wget
+"""
+This module extracts bgp data from netsage, calculates number of events from archived files
+and writes the computed data to Analysis.csv and Analysis.json files
+"""
 import os
 import json
 import time
 import datetime
 import sys
 import bz2
-from elasticsearch import Elasticsearch
-from getUrls import extrcatUrl
-from parseUpdate import parse
-from write_to_files import write_to_csv,write_to_json
 from ast import literal_eval
+import wget
+from elasticsearch import Elasticsearch
+from bgpReport_source.get_urls import extrcat_url
+from bgpReport_source.parse_update import parse
+from bgpReport_source.write_to_files import write_to_csv, write_to_json
 
-# Converts from readable form year-month-day-hour-min-sec to unix time 
-def getUnixTime(dt):
-       	splitDate = dt.split('-')
-        mydat = datetime.datetime(int(splitDate[0]),int(splitDate[1]),int(splitDate[2]),int(splitDate[3]),int(splitDate[4]),int(splitDate[5]))
-        epoch = datetime.datetime.utcfromtimestamp(0)
-        td = mydat - epoch
-        unixtime = (td.microseconds + (td.seconds + td.days * 86400) * 10 ** 6 )/ 10**6
-        unixStr = str(unixtime)
-        if len(unixStr) <13:
-                for i in range (13 - len(unixStr)):
-                        unixStr = unixStr + "0"
-        return int(unixStr)
+# Converts from readable form year-month-day-hour-min-sec to unix time
+def get_unix_time(date):
+	split_date = date.split('-')
+	mydat = datetime.datetime(int(split_date[0]), int(split_date[1]), int(split_date[2]),\
+		int(split_date[3]), int(split_date[4]), int(split_date[5]))
+	epoch = datetime.datetime.utcfromtimestamp(0)
+	time_difference = mydat - epoch
+	unixtime = (time_difference.microseconds + (time_difference.seconds + time_difference.days * 86400) * 10 ** 6)/ 10**6
+	unix_str = str(unixtime)
+	if len(unix_str) < 13:
+		for i in range(13 - len(unix_str)):
+			unix_str = unix_str + "0"
+	return int(unix_str)
 
 # Get flow entries in the range[start,end] using ES API
-def getFlowEntries(start,end,ES_Instance):
-	es = Elasticsearch([ES_Instance])
-	#Get top 10 IP group by total data bits sent - 
-	query = {"query":{"range": {"start": {"gte": start,"lte":end,"format":"epoch_millis"}}},\
-		"aggs":{"group_by_src_ip":{"terms":{"field":"meta.src_ip"}, "aggs":{"total_bits":{"sum":{"field":"values.num_bits"}}}}}}
-	return es.search(body=query,scroll='1m')["aggregations"]["group_by_src_ip"]["buckets"]
+def get_flow_entries(start, end, es_instance):
+	es_object = Elasticsearch([es_instance])
+	#Get top 10 IP group by total data bits sent -
+	query = {"query":{"range": {"start": {"gte":start, "lte":end, "format":"epoch_millis"}}},\
+		"aggs":{"group_by_src_ip":{"terms":{"field":"meta.src_ip"}, "aggs":{"total_bits":\
+		{"sum":{"field":"values.num_bits"}}}}}}
+	return es_object.search(body=query, scroll='1m')["aggregations"]["group_by_src_ip"]["buckets"]
 
 def write_status(error=0, error_text=""):
-		open("status.json",'w').close() # to clear contents of the file
-                status_file = open("status.json","w")
-                status_file.seek(0)
-                status_obj = {"timestamp":datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f'),"error_text":error_text,"error":error}
-                status_file.write(json.dumps(status_obj))
-                status_file.close()
+	open("status.json", 'w').close() # to clear contents of the file
+	status_file = open("status.json", "w")
+	status_file.seek(0)
+	status_obj = {"timestamp":datetime.datetime.fromtimestamp(time.time())\
+		     .strftime('%Y-%m-%d %H:%M:%S.%f'), "error_text":error_text, "error":error}
+	status_file.write(json.dumps(status_obj))
+	status_file.close()
 
 def extract_top_talkers(nflow):
-	topTalkers=[]
+	top_talkers = []
 	for each in nflow:
 		if each["key"] != "":
-			topTalkers.append((each["key"],each["total_bits"]["value"]))
-	return topTalkers
+			top_talkers.append((each["key"], each["total_bits"]["value"]))
+	return top_talkers
 
-def main(START_TIME =  datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(2), '%Y-%m-%d-%H-%M-%S'), END_TIME =  datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), '%Y-%m-%d-%H-%M-%S')):
+def main(START_TIME=datetime.datetime.strftime(datetime.datetime.now()\
+	- datetime.timedelta(2), '%Y-%m-%d-%H-%M-%S'),\
+	END_TIME=datetime.datetime.strftime(datetime.datetime.now()\
+	- datetime.timedelta(1), '%Y-%m-%d-%H-%M-%S')):
 	try:
-		print "Process started at - ",datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f')
-		print "start - ",START_TIME
-		print "end - ",END_TIME
-		config_file = open("config.json","r")
-		config_obj = literal_eval(config_file.read())
-	        nflow = getFlowEntries(getUnixTime(START_TIME),getUnixTime(END_TIME),config_obj["ES_Instance"])
-		topTalker_sources = extract_top_talkers(nflow)
-		print topTalker_sources
-		url_list= extrcatUrl([START_TIME,END_TIME])
+		print "Process started at - ", datetime.datetime.fromtimestamp(time.time())\
+		.strftime('%Y-%m-%d %H:%M:%S.%f')
+		print "start - ", START_TIME
+		print "end - ", END_TIME
+		config_obj = literal_eval(open("config.json", "r").read())
+		nflow = get_flow_entries(get_unix_time(START_TIME), get_unix_time(END_TIME), config_obj["ES_Instance"])
+		top_talker_sources = extract_top_talkers(nflow)
+		print top_talker_sources
+		url_list = extrcat_url([START_TIME, END_TIME])
 		print url_list
 		pwd = os.getcwd()
-		flapsDict={}
-		for eachFile in url_list:
-			file_name = wget.download(eachFile)
-			decompressed_file = open(file_name[:-4],"a+")
-			print "file name ---  ",file_name
-			preF = parse(bz2.BZ2File(pwd+"/"+file_name,"rb"),topTalker_sources)
-			for key,value in preF.iteritems():
-				if key in flapsDict:
-                                	flapsDict[key] = flapsDict[key] + value
-                     		else:
-					print "key not in flapsDict ",key
-                        		flapsDict[key] = value
-		
-		write_to_csv(flapsDict, topTalker_sources, START_TIME)
-		write_to_json(flapsDict, topTalker_sources, START_TIME)
-		
-		#Removing update files - 
+		flaps_dict = {}
+		for each_file in url_list:
+			file_name = wget.download(each_file)
+			print "file name ---  ", file_name
+			parsed_files = parse(bz2.BZ2File(pwd+"/"+file_name, "rb"), top_talker_sources)
+			for key, value in parsed_files.iteritems():
+				if key in flaps_dict:
+					flaps_dict[key] = flaps_dict[key] + value
+				else:
+					print "key not in flaps_dict ", key
+					flaps_dict[key] = value
+
+		write_to_csv(flaps_dict, top_talker_sources, START_TIME)
+		write_to_json(flaps_dict, top_talker_sources, START_TIME)
+
+		#Removing update files -
 		for fname in os.listdir(pwd):
-		    if fname.startswith("updates"):
-        		os.remove(os.path.join(pwd, fname))
-		
+			if fname.startswith("updates"):
+				os.remove(os.path.join(pwd, fname))
+
 		write_status()
 	except Exception as e:
-		print "Exception -",e
-		write_status(1,str(e))
+		print "Exception -", e
+		write_status(1, str(e))
+
--- bgpReport_source/bgpReport_source/write_to_files.py
+++ bgpReport_source/bgpReport_source/write_to_files.py
@@ -1,56 +1,54 @@
-import ast
 import csv
 import commands
-import sys
 import json
 
-def write_to_csv(flapsDict, topTalker_sources, START_TIME):
-	csvFile = open("Analysis.csv", "a")
-	file_to_write = csv.writer(csvFile, delimiter=',')
-	Date = START_TIME[0:10]
-	for line2 in topTalker_sources:
-		listToWrite = []
-		listToWrite.append(Date)
-		listToWrite.append(line2[0])
-		listToWrite.append(line2[1])
-		ip = listToWrite[1]
-		ip = ip[:ip.find("x")]+"0/24"
-		listToWrite.append(flapsDict[ip])
-		cmd = "whois -h whois.radb.net "+ip+" | grep descr:"
-		print "ip is -- ",ip
+def write_to_csv(flaps_dict, top_talker_sources, start_time):
+	csv_file = open("Analysis.csv", "a")
+	file_to_write = csv.writer(csv_file, delimiter=',')
+	date = start_time[0:10]
+	for line2 in top_talker_sources:
+		list_to_write = []
+		list_to_write.append(date)
+		list_to_write.append(line2[0])
+		list_to_write.append(line2[1])
+		ip_address = list_to_write[1]
+		ip_address = ip_address[:ip_address.find("x")]+"0/24"
+		list_to_write.append(flaps_dict[ip_address])
+		cmd = "whois -h whois.radb.net "+ip_address+" | grep descr:"
+		print "ip is -- ", ip_address
 		try: #When no records were found for particular IP, ignore them
 			descr = commands.getoutput(cmd).split("\n")[0].split(":")[1].strip(" ")
-			listToWrite.append(descr)
-		except Exception as e:
-			print ("write to cs exception ",e)
-			listToWrite.append("NOT FOUND IN RADb")
-		print listToWrite
-		file_to_write.writerow(listToWrite)
+			list_to_write.append(descr)
+		except IndexError as e:
+			print ("write to cs exception ", e)
+			list_to_write.append("NOT FOUND IN RADb")
+		print list_to_write
+		file_to_write.writerow(list_to_write)
 	file_to_write.writerow([])
-	csvFile.close()
+	csv_file.close()
 
-def write_to_json(flapsDict, topTalker_sources, START_TIME):
-	open("Analysis.json",'w').close() # to clear contents of the file  
-        file_to_write = open("Analysis.json","w")
-	Date = START_TIME[0:10]
-        list_file = []
-        for line2 in topTalker_sources:
-                listToWrite = {"Date":"","Prefix":"","DataSentInbits":"","Events":"","Organization":""}
-                listToWrite["Date"] = Date
-                listToWrite["Prefix"] = line2[0]
-                listToWrite["DataSentInbits"] = int(line2[1])
-                ip = line2[0]
-                ip = ip[:ip.find("x")]+"0/24"
-                listToWrite["Events"] = flapsDict[ip]
-                cmd = "whois -h whois.radb.net "+ip+" | grep descr:"
-                try:
-                        descr = commands.getoutput(cmd).split("\n")[0].split(":")[1].strip(" ")
-                        listToWrite["Organization"] = descr
-                except Exception as e:
-			print "write to json exception ",e
-                        listToWrite["Organization"] = "NOT FOUND IN RADb"
-                print listToWrite
-                list_file.append(listToWrite)
-        file_to_write.seek(0)
-        file_to_write.write(json.dumps(list_file))
-        file_to_write.close()
+def write_to_json(flaps_dict, top_talker_sources, start_time):
+	open("Analysis.json", 'w').close() # to clear contents of the file
+	file_to_write = open("Analysis.json", "w")
+	date = start_time[0:10]
+	list_file = []
+	for line2 in top_talker_sources:
+		list_to_write = {"Date":"", "Prefix":"", "DataSentInbits":"", "Events":"", "Organization":""}
+		list_to_write["Date"] = date
+		list_to_write["Prefix"] = line2[0]
+		list_to_write["DataSentInbits"] = int(line2[1])
+		ip_address = line2[0]
+		ip_address = ip_address[:ip_address.find("x")]+"0/24"
+		list_to_write["Events"] = flaps_dict[ip_address]
+		cmd = "whois -h whois.radb.net "+ip_address+" | grep descr:"
+		try:
+			descr = commands.getoutput(cmd).split("\n")[0].split(":")[1].strip(" ")
+			list_to_write["Organization"] = descr
+		except IndexError as e:
+			print "write to json exception ", e
+			list_to_write["Organization"] = "NOT FOUND IN RADb"
+		print list_to_write
+		list_file.append(list_to_write)
+	file_to_write.seek(0)
+	file_to_write.write(json.dumps(list_file))
+	file_to_write.close()
